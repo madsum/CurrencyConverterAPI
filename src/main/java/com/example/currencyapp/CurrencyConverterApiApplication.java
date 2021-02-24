e4d3ac861542192959c8c006a10c3aba00bdfbce
Masum Islam <madsum@gmail.com>
2021-02-24 11:35:29 +0200

    Add necessary library and refactor

--- src/main/java/com/example/currencyapp/CurrencyConverterApiApplication.java
+++ src/main/java/com/example/currencyapp/CurrencyConverterApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.currencyconverterapi;
+package com.example.currencyapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
