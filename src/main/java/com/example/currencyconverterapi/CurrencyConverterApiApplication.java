b13d6065084c4dedf1e155501645667a1ab175b0
Masum Islam <madsum@gmail.com>
2021-02-24 11:29:04 +0200

    Add generated spring boot project.

---
+++ src/main/java/com/example/currencyconverterapi/CurrencyConverterApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.currencyconverterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConverterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConverterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/currencyconverterapi/CurrencyConverterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currencyconverterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
